689e3f98ad83c4dcdb1bcbb785c703c0ca5aa32d
95ktsmith <95ktsmith@gmail.com>
2020-12-02 00:46:52 +0000

    Initial commit.

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Matrix operations """
+import numpy as np
+
+
+def np_elementwise(mat1, mat2):
+    """ Performs element-wise operations with mat1 and mat2, and returns
+        a tuple of (sum, difference, product, quotient)
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
