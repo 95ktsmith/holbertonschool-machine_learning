7e749f77284c5c8ec8acd7124531d780ea7220bd
95ktsmith <95ktsmith@gmail.com>
2020-12-02 00:55:00 +0000

    Initial commit.

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Multiply numpy ndarrays """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ Returns the result of mat1 * mat2 as a new ndarray """
+    return np.matmul(mat1, mat2)
