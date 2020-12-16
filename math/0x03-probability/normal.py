32715cdaea5c7c077aeec0479aa84e4662135a01
95ktsmith <95ktsmith@gmail.com>
2020-12-16 15:30:42 +0000

    Initial commit.

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Normal distribution """
+
+
+class Normal:
+    """ Class representing a normal distribution """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ data is a list of the data to be used to estimate the distribution
+            mean is the mean of the distribution
+            stddev is the standard deviation of the distribution
+        """
+        if stddev <= 0:
+            raise ValueError("stddev must be a positive value")
+        self.stddev = float(stddev)
+
+        if data is None:
+            self.mean = float(mean)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+
+            sum_diff = 0
+            for n in data:
+                sum_diff += (n - self.mean) ** 2
+            self.stddev = float((sum_diff / len(data)) ** 0.5)
