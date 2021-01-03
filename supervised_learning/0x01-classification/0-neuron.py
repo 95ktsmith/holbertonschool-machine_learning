2d09b215dff3a8eae0ce9d8657bbc85ea7406182
95ktsmith <95ktsmith@gmail.com>
2021-01-03 23:02:42 +0000

    Initial commit.

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Neuron """
+import numpy as np
+
+
+class Neuron:
+    """ Class to represent a neuron performing binary classification """
+    def __init__(self, nx):
+        """ nx is the number of input features to the neuron """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
