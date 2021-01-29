Fixed padding calculation.

math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -22,8 +22,8 @@ def convolve_grayscale_same(images, kernel):
     ih = images.shape[1]
     iw = images.shape[2]
     convolved = np.zeros((images.shape[0], ih, iw))
-    ph = int((kh - 1) / 2)
-    pw = int((kw - 1) / 2)
+    ph = int(kh / 2)
+    pw = int(kw / 2)
     padded = np.pad(images,
                     ((0, 0), (ph, ph), (pw, pw)),
                     'constant')