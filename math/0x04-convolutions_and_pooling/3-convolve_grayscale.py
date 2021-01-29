b50ec7844c5957f7e792e8660048ac7cb1f0cb85
95ktsmith <95ktsmith@gmail.com>
2021-01-29 16:08:14 +0000

    Fixed output shape calculation.

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -46,8 +46,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
                     'constant')
     sh = stride[0]
     sw = stride[1]
-    ch = int((padded.shape[1] - kh + 1) / sh)
-    cw = int((padded.shape[2] - kw + 1) / sw)
+    ch = int(((padded.shape[1] - kh) / sh) + 1)
+    cw = int(((padded.shape[2] - kw) / sw) + 1)
     convolved = np.zeros((images.shape[0], ch, cw))
 
     for row in range(ch):
