3859dad22e346b985487980c8af43d2248fc47d8
95ktsmith <95ktsmith@gmail.com>
2021-02-11 21:27:59 +0000

    Initial commit.

---
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Transition layer """
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """
+    Builds a transition layer
+    X is the output from the previous layer
+    nb_filters is an integer representing the number of filters in X
+    compression is the compression factor for the transition layer
+    All weights use he normal intialization
+    Returns the output of the transition layer and the number of filters
+        within the output, respectively
+    """
+    norm = K.layers.BatchNormalization()(X)
+    relu = K.layers.Activation('relu')(norm)
+    conv = K.layers.Conv2D(
+        filters=int(nb_filters * compression),
+        kernel_size=1,
+        strides=1,
+        padding='same',
+        kernel_initializer=K.initializers.he_normal()
+    )(relu)
+    pool = K.layers.AveragePooling2D()(conv)
+    return pool, pool.shape[-1]
