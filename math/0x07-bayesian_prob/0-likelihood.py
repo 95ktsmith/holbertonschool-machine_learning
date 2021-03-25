eac89218a7aed98e4c663a603480e2eebe09ce66
95ktsmith <95ktsmith@gmail.com>
2021-03-25 17:40:45 -0400

    Error checks.

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Likelihood """
+import numpy as np
+
+
+def likelihood(x, n, p):
+    """
+    Calculates likelihood of obtaining the data, x and n, for each probability
+        in P, respectively
+    """
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndaray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    return None
