99b85c37ef0fc26cc804a76d852a97ca1efb3097
95ktsmith <95ktsmith@gmail.com>
2021-03-31 11:58:22 -0400

    Initial commit.

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Optimum Cluster Count """
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Tests for the optimum number of clusters by variance
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    kmin is a positive integer containing the minimum number of clusters to
+        check for (inclusive)
+    kmax is a positive integer containing the maximum number of clusters to
+        check for (inclusive)
+    iterations is a positive integer containing the maximum number of
+        iterations for K-means
+    Returns results, d_vars, or None, None on failure
+        results is a list of the outputs of K-means for each cluster size
+        d_vars is a list of the difference in variance from smallest cluster
+            size for each cluster size
+    """
+    if type(kmin) is not int or kmin < 1:
+        return None, None
+    if (kmax is not None and type(kmax) is not int) or kmax < 2:
+        return None, None
+
+    results = []
+    d_vars = []
+    if kmax is None:
+        kmax = 5
+
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        if C is None:
+            return None, None
+
+        var = variance(X, C)
+        if var is None:
+            return None, None
+
+        if k == kmin:
+            smallest = var
+
+        results.append((C, clss))
+        d_vars.append(smallest - var)
+
+    return results, d_vars
