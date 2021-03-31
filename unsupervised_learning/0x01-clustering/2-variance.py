0bf603fc2f9ea8a761934d3eb744a997af0b749a
95ktsmith <95ktsmith@gmail.com>
2021-03-31 11:20:46 -0400

    Matches checker output until 9+ clusters.

---
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Intra-cluster Variance """
+import numpy as np
+
+
+def variance(X, C):
+    """
+    Calculates the total intra-cluster variance for a data set
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    C is a numpy.ndarray of shape (k, d) containing the centroid means
+    Returns var, or None on failure
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(C) is not np.ndarray or len(C.shape) != 2:
+        return None
+
+    n, d = X.shape
+    k, _ = C.shape
+    points = np.repeat(X, k, axis=0).reshape((n, k, d))
+    distances = ((points - C) ** 2).sum(axis=2)
+    min_distance = np.amin(distances, axis=1)
+    var = np.sum(min_distance)
+    return var
