621731ba4db1c176b41ef412a0a0184cb3aa5043
95ktsmith <95ktsmith@gmail.com>
2021-04-01 16:24:47 -0400

    Initial commit, error checks.

---
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Expecation Maximization """
+import numpy as np
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
+
+
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
+    """
+    Performs expectation maximization for a GMM
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None, None, None, None
+    if type(k) is not int or k < 1 or k > X.shape[0]:
+        return None, None, None, None, None
+    if type(iterations) is not int or iterations < 1:
+        return None, None, None, None, None
+    if type(tol) is not float or tol < 0:
+        return None, None, None, None, None
+    if type(verbose) is not bool:
+        return None, None, None, None, None
+
+    return None, None, None, None, None
