d91895ec20ed7ba29d0e3cd02dbcc2aa318d8f28
95ktsmith <95ktsmith@gmail.com>
2021-04-08 23:04:32 -0400

    Initial commit.

---
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Viterbi Algorithm """
+import numpy as np
+
+
+def viterbi(Observation, Emission, Transition, Initial):
+    """
+    Calculates the ost likely sequence of hidden states for a hidden
+        markov model
+    """
+    return None, None
