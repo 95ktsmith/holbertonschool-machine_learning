38299d8f9c62010fa72c7787787fe1de1d80777d
95ktsmith <95ktsmith@gmail.com>
2021-04-22 09:37:29 -0400

    Fixed import K to keras, using Model instead of Sequential for auto.

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import tensorflow.keras as K
+import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
@@ -11,47 +11,51 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         auto is the fuller autoencoder model
     """
     # Build encoder
-    enc_input = K.Input(shape=(input_dims,))
-    enc_layer = K.layers.Dense(hidden_layers[0], activation='relu')(enc_input)
+    enc_input = keras.Input(shape=(input_dims,))
+    enc_layer = keras.layers.Dense(
+        hidden_layers[0],
+        activation='relu'
+    )(enc_input)
     for i in range(1, len(hidden_layers)):
-        enc_layer = K.layers.Dense(
+        enc_layer = keras.layers.Dense(
             hidden_layers[i],
             activation='relu'
         )(enc_layer)
 
     # Final encoder layer
-    enc_output = K.layers.Dense(
+    enc_output = keras.layers.Dense(
         latent_dims,
         activation='relu'
     )(enc_layer)
 
     # Encoder model
-    encoder = K.Model(inputs=enc_input, outputs=enc_output)
+    encoder = keras.Model(inputs=enc_input, outputs=enc_output)
 
     # Build decoder
-    dec_input = K.Input(shape=(latent_dims,))
-    dec_layer = K.layers.Dense(hidden_layers[-1], activation='relu')(dec_input)
+    dec_input = keras.Input(shape=(latent_dims,))
+    dec_layer = keras.layers.Dense(
+        hidden_layers[-1],
+        activation='relu'
+    )(dec_input)
     for i in range(len(hidden_layers) - 2, -1, -1):
-        dec_layer = K.layers.Dense(
+        dec_layer = keras.layers.Dense(
             hidden_layers[i],
             activation='relu'
         )(dec_layer)
 
     # Final decoder layer
-    dec_output = K.layers.Dense(
+    dec_output = keras.layers.Dense(
         input_dims,
         activation='sigmoid'
     )(dec_layer)
 
     # Decoder model
-    decoder = K.Model(inputs=dec_input, outputs=dec_output)
+    decoder = keras.Model(inputs=dec_input, outputs=dec_output)
 
     # Combine into full autoencoder
-    auto = K.Sequential()
-    auto.add(encoder)
-    auto.add(decoder)
+    auto = keras.Model(enc_input, decoder(encoder(enc_input)))
     auto.compile(
-        optimizer=K.optimizers.Adam(),
+        optimizer=keras.optimizers.Adam(),
         loss="binary_crossentropy"
     )
 
