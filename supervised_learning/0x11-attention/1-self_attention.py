076c91901a0c5cfb9fd6c806b46e56a937c8088d
95ktsmith <95ktsmith@gmail.com>
2021-05-20 16:05:26 -0400

    Initial commit.

---
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Self Attention """
+import tensorflow as tf
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    """
+    Class to calculate the attention for machine translation
+    """
+    def __init__(self, units):
+        """
+        units: an integer representing the number of hidden units in the
+               alignment model
+        """
+        super().__init__()
+        self.W = tf.layers.Dense(units)
+        self.U = tf.layers.Dense(units)
+        self.V = tf.layers.Dense(1)
+
+    def call(self, s_prev, hidden_states):
+        """
+        s_prev: tensor of shape (batch, units) containing the previous decoder
+                hidden state
+        hidden_states: tensor of shape *(batch, input_seq_len, units)
+                       containing the outputs of the encoder
+        Returns: context, weights
+                 context: tensor of shape (batch, units) that contains the
+                          context vector for the decoder
+                 weights: tensor of shape (batch, input_seq_len, 1) that
+                          contains the attention weights
+        """
+        prev_reshape = tf.expand_dims(s_prev, axis=1)
+
+        # Attention weights
+        A = self.V(tf.nn.tanh(self.W(prev_reshape) + self.U(hidden_states)))
+        weights = tf.nn.softmax(A, axis=1)
+
+        # Context
+        context = tf.reduce_sum(weights * hidden_states, axis=1)
+
+        return context, weights
