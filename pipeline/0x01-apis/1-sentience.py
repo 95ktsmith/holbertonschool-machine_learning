42ac6bbbf9e31215063dc122fdec65acdb56c45f
95ktsmith <95ktsmith@gmail.com>
2021-07-26 17:18:45 -0400

    Iterate through species instead of people -> species

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -7,22 +7,19 @@ def sentientPlanets():
     """
     Returns the list of names of the home planets of all sentient species
     """
-    url = "https://swapi-api.hbtn.io/api/people"
+    url = "https://swapi-api.hbtn.io/api/species"
     planets = []
-    checkedSpecies = []
 
     while url is not None:
         data = requests.get(url).json()
 
-        for person in data['results']:
-            for species in person['species']:
-                if species not in checkedSpecies:
-                    checkedSpecies.append(species)
-                    speciesData = requests.get(species).json()
-                    if speciesData['designation'] == 'sentient':
-                        homeWorld = requests.get(person['homeworld']).json()
-                        if homeWorld['name'] not in planets:
-                            planets.append(homeWorld['name'])
+        for species in data['results']:
+            if (species['designation'] == 'sentient' or
+                    species['classification'] == 'sentient') and\
+                    species['homeworld'] is not None:
+                planetData = requests.get(species['homeworld']).json()
+                if planetData['name'] not in planets:
+                    planets.append(planetData['name'])
 
         url = data['next']
 
